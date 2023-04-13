bac2ae3d85e5e1eec8b5928daa2abcf35f9b11a1
EricLam404 <lameric9888@gmail.com>
2023-04-12 21:54:20 -0400

    Changed Schedule to hold days

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,11 +1,69 @@
 import React, { useEffect, useState} from 'react';
+import "../styles/Schedule.css";
 
 function Schedule({schedule}) {
     return (
-        <div className="student-list">
+        <div className="schedule-list">
             <div className='name'>Student List</div>
             {schedule ? (
-                <ul>
+                <div className='week-list'>
+                    <div className='monday'>
+                        <div className='day-of-week'>Monday</div>
+                        <ul>
+                            {
+                                const mondayStudents = schedule.filter()
+                            }
+                        </ul>
+                    </div>
+                    <div className='tuesday'>
+                        <div className='day-of-week'>Tuesday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                    <div className='wednesday'>
+                        <div className='day-of-week'>Wednesday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                    <div className='thursday'>
+                        <div className='day-of-week'>Thursday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                    <div className='friday'>
+                        <div className='day-of-week'>Friday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                    <div className='saturday'>
+                        <div className='day-of-week'>Saturday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                    <div className='sunday'>
+                        <div className='day-of-week'>Sunday</div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                </div>
+            ) :
+            (
+                <div>loading students</div>
+            )}
+        </div>
+    );
+}
+
+export default Schedule;
+
+/*
+<ul>
                     {schedule.map((item, index) => (
                         <li className='time' key={index}>
                         <div className='class-block'>
@@ -21,12 +79,4 @@ function Schedule({schedule}) {
                     </li>
                     ))}
                 </ul>
-            ) :
-            (
-                <div>loading students</div>
-            )}
-        </div>
-    );
-}
-
-export default Schedule;
+                */
--- server/index.js
+++ server/index.js
@@ -33,197 +33,60 @@ app.get("/student", (req, res) => {
 
 app.listen(5001, () => {console.log("Server started on port 5001")});
 
-
-/*
-
-app.use(express.static("../client/build"));
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT);
-*/
-
-
-
-function API(){
-    return [
-        {
-            time: "11am",
-            students: [{
-                name: "Student #1",
-                electronics: {
-                    started: false,
-                    classes: []
-                },
-                coding: {
-                    started: false,
-                    classes: []
-                },
-                robotics: {
-                    started: true,
-                    classes: [
-                        {
-                            classNumber: 1,
-                            classDate: "4/4/2023",
-                            classAchievement: "started lesson 1",
-                            classLesson: "Lesson 1"
-                        },
-                        {
-                            classNumber: 2,
-                            classDate: "4/5/2023",
-                            classAchievement: "started lesson 2",
-                            classLesson: "Lesson 2"
-                        }
-                    ]
-                }
+function createStudent(name, day, time){
+    const student = new Student({
+        name: name,
+        email: name + '@gmail.com',
+        dob: '01/01/2001',
+        age: 23,
+        phone: '123-123-1234',
+        address: '123 Main St, USA',
+        started: new Date(),
+        days: [
+            {
+                day: day,
+                time: time
             },
             {
-                name: "Student #2",
-                electronics: {
-                    started: false,
-                    classes: []
-                },
-                coding: {
-                    started: false,
-                    classes: []
-                },
-                robotics: {
-                    started: true,
-                    classes: [
-                        {
-                            classNumber: 1,
-                            classDate: "4/4/2023",
-                            classAchievement: "started lesson 1",
-                            classLesson: "Lesson 1"
-                        },
-                        {
-                            classNumber: 2,
-                            classDate: "4/5/2023",
-                            classAchievement: "started lesson 2",
-                            classLesson: "Lesson 2"
-                        }
-                    ]
-                }
-            }]
-        },
-        {
-            time: "12pm",
-            students: [{
-                name: "Student #3",
-                electronics: {
-                    started: false,
-                    classes: []
-                },
-                coding: {
-                    started: false,
-                    classes: []
-                },
-                robotics: {
-                    started: true,
-                    classes: [
-                        {
-                            classNumber: 1,
-                            classDate: "4/4/2023",
-                            classAchievement: "started lesson 1",
-                            classLesson: "Lesson 1"
-                        },
-                        {
-                            classNumber: 2,
-                            classDate: "4/5/2023",
-                            classAchievement: "started lesson 2",
-                            classLesson: "Lesson 2"
-                        }
-                    ]
-                }
-            },
+                day: "Saturday",
+                time: "11:00"
+            }
+        ],
+        classes: {
+        robotics: [
             {
-                name: "Student #4",
-                electronics: {
-                    started: false,
-                    classes: []
-                },
-                coding: {
-                    started: false,
-                    classes: []
-                },
-                robotics: {
-                    started: true,
-                    classes: [
-                        {
-                            classNumber: 1,
-                            classDate: "4/4/2023",
-                            classAchievement: "started lesson 1",
-                            classLesson: "Lesson 1"
-                        },
-                        {
-                            classNumber: 2,
-                            classDate: "4/5/2023",
-                            classAchievement: "started lesson 2",
-                            classLesson: "Lesson 2"
-                        }
-                    ]
-                }
-            }]
+            classNumber: 1,
+            classDate: new Date(),
+            classAchievement: 'Completed robotics project',
+            classLevel: 'Level 1',
+            classLesson: 'Lesson 1'
+            }
+        ],
+        electronics: [
+            {
+            classNumber: 1,
+            classDate: new Date(),
+            classAchievement: 'Completed electronic project',
+            classLevel: 'Level 1',
+            classLesson: 'Lesson 1'
+            }
+        ],
+        coding: [
+            {
+            classNumber: 1,
+            classDate: new Date(),
+            classAchievement: 'Completed coding project',
+            classLevel: 'Level 2',
+            classLesson: 'Lesson 1'
+            }
+        ]
         }
-    ];
+    });
+    student.save((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Student saved successfully!');
+        }
+    });
 }
-
-/*
-const student = new Student({
-    name: 'John',
-    email: 'john@example.com',
-    dob: '2000-01-01',
-    age: 23,
-    phone: '555-1234',
-    address: '123 Main St, Anytown USA',
-    started: new Date(),
-    days: [
-      {
-        day: 'Monday',
-        time: '10:00am'
-      },
-      {
-        day: 'Wednesday',
-        time: '2:00pm'
-      }
-    ],
-    classes: {
-      robotics: [
-        {
-        classNumber: 1,
-        classDate: new Date(),
-        classAchievement: 'Completed robot building project',
-        classLevel: 'Level 1',
-        classLesson: 'Advanced robotics programming'
-        }
-      ],
-      electronics: [
-        {
-        classNumber: 1,
-        classDate: new Date(),
-        classAchievement: 'Completed circuit design project',
-        classLevel: 'Level 1',
-        classLesson: 'Advanced electronics programming'
-        }
-      ],
-      coding: [
-        {
-        classNumber: 1,
-        classDate: new Date(),
-        classAchievement: 'Completed coding challenge',
-        classLevel: 'Level 2',
-        classLesson: 'Introduction to web development'
-        }
-      ]
-    }
-});
-  
-student.save((err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Student saved successfully!');
-    }
-    mongoose.disconnect();
-});
-*/

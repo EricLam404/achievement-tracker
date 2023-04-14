1793acc17abd8b7a5b30c6ea59f09a41338eb235
EricLam404 <lameric9888@gmail.com>
2023-04-14 12:58:55 -0400

    Changed filter so that it filters by day and time

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -10,17 +10,36 @@ function Schedule({schedule}) {
             <div className='week-list'>
                 {schedule ? (
                     days.map((day, index) => {
-                        const filteredData = schedule.filter(student => {
+                        const daySchedule = schedule.filter(student => {
                             return student.days.some(days => days.day === day);
                         });
+                        let times = daySchedule.map(obj => {
+                            return obj.days.filter(times => times.day === day)
+                        });
+                        times = times.map(arr => arr.map(day => day.time)).flat();
+                        times = [...new Set(times.sort())];
+                        //console.log(day);
+                        //console.log(times.length > 0 ? times : "Nothing");
                         return (
                         <div className='week' key={index}>
                             <div className='day-of-week' key={index}>{day}</div>
                             <ul>
                                 {
-                                    filteredData.map((student, index) => {
+                                    times.map((time, index) => {
+                                        let students = daySchedule.filter(student => {
+                                            return student.days.some(days => days.day === day && days.time === time)
+                                        });
                                         return (
-                                            <div className='name' key={index}>{student.name}</div>
+                                            <div className='time-box' key={index}>
+                                                <div className='time'>{time}</div>
+                                                <div className='students'>
+                                                    {students.map((student, index) => {
+                                                        return (
+                                                            <div className='name' key={index}>{student.name}</div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
                                         )
                                     })
                                 }

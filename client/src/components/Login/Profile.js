c70fcb25fb73437598d5386a2d4974e2cfa5536f
EricLam404 <lameric9888@gmail.com>
2023-06-17 21:01:46 -0400

    Added auth to fetch api

--- client/src/components/Login/Profile.js
+++ client/src/components/Login/Profile.js
@@ -2,7 +2,26 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated} = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  async function callAPI(){
+    const token = await getAccessTokenSilently()
+    const url = 'http://localhost:5001/api/student/students';
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authentication: `Bearer ${token}`
+        },
+    })
+    .then((response) => response.text())
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+  }
 
   return (
     isAuthenticated && (
@@ -12,6 +31,7 @@ const Profile = () => {
             <ul>
                 {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]}</li>)}
             </ul>     
+            <button className="btn" onClick={callAPI}>CALL API</button>
         </div>
     )
   )

50a7841f38348b55d8310076a98697bfad090f18
EricLam404 <lameric9888@gmail.com>
2023-08-06 15:14:05 -0400

    Cleaned up api endpoint for classes

--- server/routes/classes.js
+++ server/routes/classes.js
@@ -1,46 +1,49 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const Student = require('../models/student');
 
-router.post('/:classType', async (req, res) => {
-    const { id, classNumber, classDate, classAchievement, classLesson, classType, classLevel } = req.body;
-    const classTypes = ['electronics', 'robotics', 'coding'];
+const class_types = ['electronics', 'robotics', 'coding'];
 
-    if(!classTypes.includes(classType)){
-        res.status(400).send('Invalid class type!');
-        return;
-    }
-    const update = {};
-    update[`classes.${classType}`] = {
-        classNumber: classNumber,
-        classDate: classDate,
-        classAchievement: classAchievement,
-        classLesson: classLesson,
-        classLevel: classLevel
-    };
-
+router.post('/:class_type', async (req, res) => {
     try {
-        const doc = await Student.findOneAndUpdate({_id: id}, {$push: update});
+        const { student_id, class_type } = req.params;
+        const { classNumber, classDate, classAchievement, classLesson, classLevel } = req.body;
+
+        if(!class_types.includes(class_type)){
+            return res.status(400).send('Invalid class type!');
+        }
+        const update = {
+            [`classes.${class_type}`]: {
+            classNumber,
+            classDate,
+            classAchievement,
+            classLesson,
+            classLevel,
+            },
+        };
+        const doc = await Student.findOneAndUpdate({_id: student_id}, {$push: update}, { new: true });
         res.send(doc);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Error Adding Class');
+        res.status(500).send({
+            message: `Error Adding Class`,
+            error: err
+        });
     }
 });
 
-router.delete('/:classType', async (req, res) => {
-    const { studentId, classId, classType } = req.body;
-    const classTypes = ['electronics', 'robotics', 'coding'];
-
-    if(!classTypes.includes(classType)){
-        res.status(400).send('Invalid class type!');
-        return;
-    }
-    const update = {};
-    update[`classes.${classType}`] = {
-        _id: classId
-    };
+router.delete('/:class_type/:class_id', async (req, res) => {
     try {
-        const doc = await Student.findOneAndUpdate({_id: studentId}, {$pull: update});
+        const { student_id, class_type, class_id } = req.params;
+
+        if(!class_types.includes(class_type)){
+            return res.status(400).send('Invalid class type!');
+        }
+        
+        const update = {
+            [`classes.${class_type}`]: {
+                _id: class_id
+            }
+        };
+        const doc = await Student.findOneAndUpdate({_id: student_id}, {$pull: update});
         res.send(doc);
     } catch (err) {
         console.error(err.message);

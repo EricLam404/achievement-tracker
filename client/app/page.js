7619ed6542d4e71e18d0bd41cdda65eda3b01c41
EricLam404 <lameric9888@gmail.com>
2023-09-07 10:35:47 -0400

    Added auth0 authentication

--- client/app/page.js
+++ client/app/page.js
@@ -1,13 +1,25 @@
 import Footer from "@/components/main/Footer";
 import Header from "@/components/main/Header";
 import Home from "@/components/main/Home";
+import { getSession } from '@auth0/nextjs-auth0';
 
-export default function Page() {
-  return (
-    <main>
-      <Header />
-      <Home />
-      <Footer />
-    </main>
-  )
+export default async function Page() {
+    const session = await getSession();
+    const user = session ? session.user : null;
+
+    return (
+        <main>
+            {user ? 
+                <>
+                    <Header />
+                    <Home />
+                    <Footer />
+                </> :
+                <>
+                    <a className="btn" href="/api/auth/login">Login</a>
+                    <a className="btn" href="/api/auth/logout">Logout</a>
+                </>
+            }
+        </main>
+    )
 }

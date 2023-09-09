7beaf71add5dcca300d2266cab340b3acc1db099
EricLam404 <lameric9888@gmail.com>
2023-09-09 19:03:58 -0400

    Added proxy for external api

---
+++ client/app/api/proxy/route.js
@@ -0,0 +1,15 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+    const res = new NextResponse();
+    const { accessToken } = await getAccessToken(req, res);
+    const path = req.nextUrl.pathname.replace("/api/", "");
+    const EXTERNAL_API_URL = "http://localhost:5001";
+
+    return await fetch(`https:/${EXTERNAL_API_URL}/${path}${req.nextUrl.search}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}

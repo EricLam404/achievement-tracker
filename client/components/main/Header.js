a9b4bf4c49fe627dbe63cb06f500b66c572dce3e
EricLam404 <lameric9888@gmail.com>
2023-09-16 09:54:18 -0400

    Add hyperlink

--- client/components/main/Header.js
+++ client/components/main/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Header() {
     return (
         <div className="flex justify-between px-20 items-center h-20 text-white bg-gradient-to-r from-teal-400 via-blue-400 to-teal-400 text-shadow-sm shadow-sm">
-            <div className="text-2xl font-bold">Achievement Tracker</div>
+            <a href="/" className="text-2xl font-bold">Achievement Tracker</a>
             <Link href="/profile">
                 <div className="text-xl font-semibold hover:text-gray-200">
                     Profile
